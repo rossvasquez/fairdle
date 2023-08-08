c80e3d66ed095ff02d15ab406ddc96ac17f22b0f
rossvasquez <ross.vasquez.17@gmail.com>
2023-08-08 13:47:35 -0500

    Updates

--- src/assets/components/game.jsx
+++ src/assets/components/game.jsx
@@ -69,14 +69,53 @@ export default function Game() {
         }
     }
 
-    const checkExistence = (letter) => {
+    const checkExistence = (letter, alreadyExists) => {
         let exists = false
+        let count = 0
         for(let i=0;i<Answer.length; i++) {
             if (letter == Answer[i]) {
-                exists = true
+                count++
+            }
+        }
+        for(let i=0;i<Answer.length; i++) {
+            if (letter == Answer[i]) {
+                let aExists = false
+                for (let j=0;j<alreadyExists.length; j++) {
+                    if (alreadyExists[j] == letter && count<2) {
+                        aExists = true
+                    }
+                }
+                if (!aExists) {
+                    exists = true
+                }
             }
         }
         return exists
+    }
+
+    const checkBack = (tempResultsRow) => {
+        let tempArr = [...tempResultsRow]
+        let count = 0
+        for(let i=0;i<tempResultsRow.length;i++) {
+            if (tempResultsRow[i] == 1) {
+                let letter = Guesses[CurrentRow][i]
+                for(let j=0;j<Guesses[CurrentRow].length; j++) {
+                    if (letter == Guesses[CurrentRow][j]) {
+                        count++
+                    }
+                }
+                if (count > 1) {
+                    for(let k=0;k<tempResultsRow.length;k++) {
+                        if (tempResultsRow[k] == 2) {
+                            if (Answer[k] == letter) {
+                                tempArr[i] = 3
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return tempArr
     }
 
     const checkIfWord = async (thisWord) => {
@@ -115,10 +154,10 @@ export default function Game() {
             setCurrentRow(previous => previous + 1)
             setCurrentPosition(0)
         } else if (!isFull) {
-            setMessage('Please enter a full word')
+            setMessage('Try Again')
             setShowMessage(true)
         } else if (!isAWord) {
-            setMessage('Please enter a real word')
+            setMessage('Try Again')
             setShowMessage(true)
         }
     }
@@ -126,20 +165,22 @@ export default function Game() {
     const checkLetters = () => {
         let tempResults = [...Results]
         let tempResultsRow = []
+        let alreadyExists = []
         let winCount = 0
         for(let i= 0; i<Guesses[CurrentRow].length; i++) {
             let currentLetter = Guesses[CurrentRow][i]
             let correctLetter = Answer[i]
             if (currentLetter == correctLetter) {
+                alreadyExists.push(currentLetter)
                 tempResultsRow.push(2)
                 winCount++
-            } else if (checkExistence(currentLetter)) {
+            } else if (checkExistence(currentLetter, alreadyExists)) {
                 tempResultsRow.push(1)
             } else {
                 tempResultsRow.push(3)
             }
         }
-        tempResults[CurrentRow] = tempResultsRow
+        tempResults[CurrentRow] = checkBack(tempResultsRow)
         setResults(tempResults)
         if (winCount == 5) {
             setMessage('You Win!')
@@ -177,16 +218,16 @@ export default function Game() {
     }
 
     const ShareResult = () =>
-    <div className="jump flex justify-center items-center flex-col absolute h-[15rem] w-[15rem] bg-[#2e2e2e] rounded-xl bg-opacity-[97%]">
-        <p className="text-3xl text-[#ffffff] font-bold mb-14">Share It Up!</p>
-        <div onClick={() => copyResult()} className={`w-9/12 flex justify-center items-center bg-green-500 ${Copied ? 'bg-opacity-70' : 'bg-opacity-100'} py-4 rounded-full text-xl text-[#ffffff] font-semibold`}>{Copied ? 'Copied!' : 'Copy Results'}</div>
+    <div className="-bottom-4 left-0 right-0 flex justify-center items-center flex-col absolute h-[15rem] mx-auto w-full max-w-[20rem] bg-[#2e2e2e] rounded-xl z-10">
+        <p className="text-3xl text-[#ffffff] font-bold mb-8">Share It Up!</p>
+        <div onClick={() => copyResult()} className={`w-9/12 flex justify-center items-center bg-green-500 ${Copied ? 'bg-opacity-40' : 'bg-opacity-100'} py-4 rounded-full text-xl text-[#ffffff] font-semibold`}>{Copied ? 'Copied!' : 'Copy Results'}</div>
+        {Copied ? <p className="text-white text-center px-4 mt-4">Now paste your results into a text message or your favorite social media.</p> : null}
     </div>
 
     const GuessTile = () =>
-    <div className="relative h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center">
-    {Finished ? <ShareResult /> : null}
+    <div className="relative h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center px-2 mt-4">
     {Guesses.map((item, index) =>
-        <div key={index} className="flex flex-row h-auto w-auto gap-1.5 mb-1.5">
+        <div key={index} className="flex flex-row h-auto w-full justify-center gap-1.5 mb-1.5">
             {item.map((tile, i) =>
             <div key={i} className={`flex justify-center items-center text-4xl pb-1 font-bold text-[#ffffff] h-16 w-16 ${Guesses[index][i] == '' ? 'border-[#404040]' : 'border-[#757575]'} ${Results[index][i] == 1 ? 'bg-amber-400' : null} ${Results[index][i] == 2 ? 'bg-green-500' : null} ${Results[index][i] == 3 ? 'bg-[#404040]' : null} ${CurrentRow > index ? null : 'border-2'}`}>
                 {tile}
@@ -197,9 +238,9 @@ export default function Game() {
     </div>
 
     const Keyboard = () =>
-    <div className="h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center mt-4">
+    <div className={`${Finished ? 'opacity-0' : null} h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center mt-4 px-2 relative z-20 bg-[#1c1c1c]`}>
         {Keys.map((row, index) =>
-            <div key={index} className="flex flex-row h-auto w-auto gap-1 mb-1">
+            <div key={index} className="flex flex-row justify-center h-auto w-full gap-1 mb-1">
                 {row.map((letter, i) =>
                     <div key={i} onClick={Finished ? null : () => handleLetter(letter)} className="hover:cursor-pointer h-12 w-8 bg-[#757575] text-[#ffffff] font-bold flex justify-center items-center rounded-[.25rem]">
                         {letter}
@@ -210,18 +251,19 @@ export default function Game() {
     </div>
 
     const ActionButtons = () =>
-    <div className="h-auto w-full max-w-xl flex flex-row mx-auto justify-center items-center mt-2 mb-6 gap-2">
-        <div onClick={Finished ? null : () => handleGuess()} className="hover: cursor-pointer w-20 h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] border-2 border-green-500">ENTER</div>
-        <div onClick={Finished ? null : () => handleRemove()} className="hover: cursor-pointer w-20 h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] border-2 border-red-400">UNDO</div>
+    <div className={`${Finished ? 'opacity-0' : null} h-auto w-full max-w-xl flex flex-row mx-auto justify-center items-center pb-6 gap-1 relative z-20 bg-[#1c1c1c]`}>
+        <div onClick={Finished ? null : () => handleGuess()} className="hover: cursor-pointer w-[7.6rem] h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] bg-[#757575]">GUESS</div>
+        <div onClick={Finished ? null : () => handleRemove()} className="hover: cursor-pointer w-[7.6rem] h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] bg-[#757575]">UNDO</div>
     </div>
 
     const MessageView = () =>
-    <div className={`h-auto w-full max-w-xl flex flex-row mx-auto text-[#ffffff] text-2xl font-light capitalize justify-center items-center my-6 px-4 block ${ShowMessage ? 'opacity-100' : 'opacity-0'}`}>
+    <div className={`absolute z-30 -top-[5.3rem] ${Finished ? 'bg-[#1c1c1c]' : 'bg-red-400'} py-3 rounded-lg border border-[#ffffff] left-0 right-0 h-auto w-screen max-w-[18rem] mx-auto flex flex-row text-[#ffffff] text-2xl font-semibold capitalize justify-center items-center px-4 block ${ShowMessage ? 'opacity-100' : 'opacity-0'}`}>
         <p>{Message}</p>
     </div>
 
     return(
-        <div className="w-screen h-auto">
+        <div className="relative w-screen h-auto">
+            {Finished ? <ShareResult /> : null}
             <MessageView />
             <GuessTile />
             <Keyboard />
--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,10 +1,13 @@
+import headerBackground from '../static/headerBackground.png'
 
+import '../styles/header.css'
 
 export default function Header() {
 
     return(
-        <div className="h-20 w-screen bg-[#1c1c1c] flex justify-center items-center border-b border-[#2e2e2e]">
-            <p className="text-white font-bold text-2xl">Iowa State Fair<span className="font-light">dle</span></p>
+        <div className="relative h-20 w-screen bg-[#1c1c1c] flex justify-center items-center">
+            <p className="textShadow relative z-20 text-[#ffffff] font-bold text-3xl">State Fair<span className="font-light">dle</span></p>
+            <img src={headerBackground} className='max-w-xl border-b border-[#ffffff] w-screen mx-auto opacity-70 bottom-0 absolute left-0 right-0 z-10' />
         </div>
     )
 }
