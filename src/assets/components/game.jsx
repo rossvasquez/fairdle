07a85550f4422a5b14906b3a14583ff22409709f
rossvasquez <ross.vasquez.17@gmail.com>
2023-08-08 21:52:34 -0500

    Ad Setup and Alg Fix

--- src/assets/components/game.jsx
+++ src/assets/components/game.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import "../styles/game.css"
 
 export default function Game() {
+
+    const [ShowGame, setShowGame] = useState(false)
 
     const [CurrentRow, setCurrentRow] = useState(0)
 
@@ -12,7 +14,7 @@ export default function Game() {
 
     const [Message, setMessage] = useState('boop')
 
-    const [Answer, setAnswer] = useState(['R','O','D','E','O'])
+    const [Answer, setAnswer] = useState(['F','R','I','E','D'])
 
     const [Failed, setFailed] = useState(false)
 
@@ -81,7 +83,7 @@ export default function Game() {
             if (letter == Answer[i]) {
                 let aExists = false
                 for (let j=0;j<alreadyExists.length; j++) {
-                    if (alreadyExists[j] == letter && count<2) {
+                    if (alreadyExists[j] == letter && count < 2) {
                         aExists = true
                     }
                 }
@@ -93,33 +95,7 @@ export default function Game() {
         return exists
     }
 
-    const checkBack = (tempResultsRow) => {
-        let tempArr = [...tempResultsRow]
-        let count = 0
-        for(let i=0;i<tempResultsRow.length;i++) {
-            if (tempResultsRow[i] == 1) {
-                let letter = Guesses[CurrentRow][i]
-                for(let j=0;j<Guesses[CurrentRow].length; j++) {
-                    if (letter == Guesses[CurrentRow][j]) {
-                        count++
-                    }
-                }
-                if (count > 1) {
-                    for(let k=0;k<tempResultsRow.length;k++) {
-                        if (tempResultsRow[k] == 2) {
-                            if (Answer[k] == letter) {
-                                tempArr[i] = 3
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return tempArr
-    }
-
     const checkIfWord = async (thisWord) => {
-        console.log(thisWord)
         let result = null
         await fetch("/.netlify/functions/check-word", {
             method: 'POST',
@@ -134,7 +110,6 @@ export default function Game() {
         .then(data => {
             result = JSON.parse(data)
         })
-        console.log(result)
         return result
     }
 
@@ -165,22 +140,47 @@ export default function Game() {
     const checkLetters = () => {
         let tempResults = [...Results]
         let tempResultsRow = []
-        let alreadyExists = []
+        let alreadyExistsGood = []
+        let alreadyExistsBad = []
         let winCount = 0
         for(let i= 0; i<Guesses[CurrentRow].length; i++) {
             let currentLetter = Guesses[CurrentRow][i]
             let correctLetter = Answer[i]
+            let count = 0
+            let pass = true
+            for(let p = 0;p<alreadyExistsBad.length;p++) {
+                if (currentLetter == alreadyExistsBad[p]) {
+                    for (let k = 0;k<Answer.length;k++) {
+                        if (currentLetter == Answer[k]) {
+                            count++
+                        }
+                    }
+                }
+            }
+            for(let p = 0;p<alreadyExistsGood.length;p++) {
+                if (currentLetter == alreadyExistsGood[p]) {
+                    for (let k = 0;k<Answer.length;k++) {
+                        if (currentLetter == Answer[k]) {
+                            count++
+                        }
+                    }
+                }
+            }
+            if (count == 1) {
+                pass = false
+            }
             if (currentLetter == correctLetter) {
-                alreadyExists.push(currentLetter)
+                alreadyExistsGood.push(currentLetter)
                 tempResultsRow.push(2)
                 winCount++
-            } else if (checkExistence(currentLetter, alreadyExists)) {
+            } else if ((checkExistence(currentLetter, alreadyExistsGood)) && pass) {
                 tempResultsRow.push(1)
+                alreadyExistsBad.push(currentLetter)
             } else {
                 tempResultsRow.push(3)
             }
         }
-        tempResults[CurrentRow] = checkBack(tempResultsRow)
+        tempResults[CurrentRow] = tempResultsRow
         setResults(tempResults)
         if (winCount == 5) {
             setMessage('You Win!')
@@ -218,18 +218,18 @@ export default function Game() {
     }
 
     const ShareResult = () =>
-    <div className="-bottom-4 left-0 right-0 flex justify-center items-center flex-col absolute h-[15rem] mx-auto w-full max-w-[20rem] bg-[#2e2e2e] rounded-xl z-10">
-        <p className="text-3xl text-[#ffffff] font-bold mb-8">Share It Up!</p>
-        <div onClick={() => copyResult()} className={`w-9/12 flex justify-center items-center bg-green-500 ${Copied ? 'bg-opacity-40' : 'bg-opacity-100'} py-4 rounded-full text-xl text-[#ffffff] font-semibold`}>{Copied ? 'Copied!' : 'Copy Results'}</div>
+    <div className="bottom-4 left-0 right-0 flex justify-center items-center flex-col absolute h-[13rem] mx-auto w-full max-w-[20rem] bg-[#2e2e2e] rounded-xl z-10">
+        <p className="text-3xl text-[#ffffff] font-bold mb-4">Share It Up!</p>
+        <div onClick={() => copyResult()} className={`w-9/12 flex justify-center items-center bg-gradient-to-br from-[#29bfd5] to-[#6ccfd3] ${Copied ? 'bg-opacity-40' : 'bg-opacity-100'} py-4 rounded-full text-xl text-[#ffffff] font-semibold`}>{Copied ? 'Copied!' : 'Copy Results'}</div>
         {Copied ? <p className="text-white text-center px-4 mt-4">Now paste your results into a text message or your favorite social media.</p> : null}
     </div>
 
     const GuessTile = () =>
-    <div className="relative h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center px-2 mt-4">
+    <div className="relative h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center px-2 mt-6">
     {Guesses.map((item, index) =>
         <div key={index} className="flex flex-row h-auto w-full justify-center gap-1.5 mb-1.5">
             {item.map((tile, i) =>
-            <div key={i} className={`flex justify-center items-center text-4xl pb-1 font-bold text-[#ffffff] h-16 w-16 ${Guesses[index][i] == '' ? 'border-[#404040]' : 'border-[#757575]'} ${Results[index][i] == 1 ? 'bg-amber-400' : null} ${Results[index][i] == 2 ? 'bg-green-500' : null} ${Results[index][i] == 3 ? 'bg-[#404040]' : null} ${CurrentRow > index ? null : 'border-2'}`}>
+            <div key={i} className={`flex justify-center items-center text-4xl pb-1 font-bold text-[#ffffff] h-16 w-16 ${Guesses[index][i] == '' ? 'border-[#404040]' : 'border-[#757575]'} ${Results[index][i] == 1 ? 'bg-amber-400' : null} ${Results[index][i] == 2 ? 'bg-green-600' : null} ${Results[index][i] == 3 ? 'bg-[#404040]' : null} ${CurrentRow > index ? null : 'border-2'}`}>
                 {tile}
             </div>
             )}
@@ -238,11 +238,11 @@ export default function Game() {
     </div>
 
     const Keyboard = () =>
-    <div className={`${Finished ? 'opacity-0 z-0' : null} h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center mt-4 px-2 relative z-20 bg-[#1c1c1c]`}>
+    <div className={`${Finished ? 'opacity-0 z-0' : 'z-20'} h-auto w-full max-w-xl flex flex-col mx-auto justify-center items-center mt-4 px-2 relative bg-[#1c1c1c]`}>
         {Keys.map((row, index) =>
             <div key={index} className="flex flex-row justify-center h-auto w-full gap-1 mb-1">
                 {row.map((letter, i) =>
-                    <div key={i} onClick={Finished ? null : () => handleLetter(letter)} className="hover:cursor-pointer h-12 w-8 bg-[#757575] text-[#ffffff] font-bold flex justify-center items-center rounded-[.25rem]">
+                    <div key={i} onClick={Finished ? null : () => handleLetter(letter)} className="hover:cursor-pointer h-12 w-8 active:bg-opacity-80 bg-[#757575] text-[#ffffff] font-bold flex justify-center items-center rounded-[.25rem]">
                         {letter}
                     </div>
                 )}
@@ -251,23 +251,38 @@ export default function Game() {
     </div>
 
     const ActionButtons = () =>
-    <div className={`${Finished ? 'opacity-0 z-0' : null} h-auto w-full max-w-xl flex flex-row mx-auto justify-center items-center pb-6 gap-1 relative z-20 bg-[#1c1c1c]`}>
-        <div onClick={Finished ? null : () => handleGuess()} className="hover: cursor-pointer w-[7.6rem] h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] bg-[#757575]">GUESS</div>
-        <div onClick={Finished ? null : () => handleRemove()} className="hover: cursor-pointer w-[7.6rem] h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] bg-[#757575]">UNDO</div>
+    <div className={`${Finished ? 'opacity-0 z-0' : 'z-20'} h-auto w-full max-w-xl flex flex-row mx-auto justify-center items-center pb-6 gap-1 relative bg-[#1c1c1c]`}>
+        <div onClick={Finished ? null : () => handleGuess()} className="active:bg-opacity-80 hover:cursor-pointer w-[7.6rem] h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] bg-[#757575]">GUESS</div>
+        <div onClick={Finished ? null : () => handleRemove()} className="active:bg-opacity-80 hover:cursor-pointer w-[7.6rem] h-10 flex justify-center items-center text-white font-bold text-[#ffffff] rounded-[.25rem] bg-[#757575]">UNDO</div>
     </div>
 
     const MessageView = () =>
-    <div className={`absolute z-30 -top-[5.3rem] ${Finished ? 'bg-[#1c1c1c]' : 'bg-red-400'} py-3 rounded-lg border border-[#ffffff] left-0 right-0 h-auto w-screen max-w-[18rem] mx-auto flex flex-row text-[#ffffff] text-2xl font-semibold capitalize justify-center items-center px-4 block ${ShowMessage ? 'opacity-100' : 'opacity-0'}`}>
+    <div className="absolute h-auto w-screen max-w-xl mx-auto">
+    <div className={`absolute z-30 -top-[5.7rem] ${Finished ? 'bg-[#1c1c1c]' : 'bg-red-400'} py-3 rounded-lg border border-[#ffffff] left-4 h-auto w-screen max-w-[18rem] mx-auto flex flex-row text-[#ffffff] text-2xl font-semibold capitalize justify-center items-center px-4 block ${ShowMessage ? 'opacity-100' : 'opacity-0'}`}>
         <p>{Message}</p>
     </div>
+    </div>
+
+    const Game = () =>
+    <>
+        {Finished ? <ShareResult /> : null}
+        <MessageView />
+        <GuessTile />
+        <Keyboard />
+        <ActionButtons />
+    </>
+
+    const Ad = () =>
+    <>
+    <div className="w-full max-w-xl mx-auto h-20 px-8 flex items-center text-xl text-white">
+        <p className="w-full text-right">Ad</p>
+    </div>
+    <div onClick={() => setShowGame(true)} className="hover:cursor-pointer active:opacity-70 mx-auto w-48 h-14 bg-gradient-to-br from-[#29bfd5] to-[#6ccfd3] rounded-full flex justify-center items-center text-white text-2xl pb-1 font-semibold">Continue</div>
+    </>
 
     return(
-        <div className="relative w-screen h-auto">
-            {Finished ? <ShareResult /> : null}
-            <MessageView />
-            <GuessTile />
-            <Keyboard />
-            <ActionButtons />
+        <div className="relative w-screen h-auto max-w-xl mx-auto">
+            {ShowGame ? <Game /> : <Ad />}
         </div>
     )
 }
--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,13 +1,32 @@
 import headerBackground from '../static/headerBackground.png'
 
+import { useState } from 'react'
+
 import '../styles/header.css'
 
 export default function Header() {
 
+    const [ShowAbout, setShowAbout] = useState(false)
+
+    const About = () =>
+    <div className="absolute bg-[#404040] w-[22.5rem] pb-[130%] max-h-[10rem] top-24 bottom-10 mx-auto left-0 right-0 rounded-lg shadow-lg z-30 px-6 py-4">
+        <div onClick={() => setShowAbout(false)} className='absolute right-6 top-1.5 text-white text-3xl font-semibold'>x</div>
+        <p className="text-white text-3xl font-bold mt-4">How To Play</p>
+        <p className="text-white text-md mt-1 font-normal">Guess the State Fair related word in 6 tries.</p>
+        <p className="text-white text-xl mt-8 font-semibold">Upon Guessing...</p>
+        <p>If a letter is green, it is in the correct spot.</p>
+        <p>If a letter is yellow, it is not in the correct spot, but exists in the word.</p>
+        <p>If a letter is grey, it does not exist in the word.</p>
+        <p>A new word will be available for each day of the State Fair.</p>
+        <a href="https://rossvasquez.netlify.app" target='_blank'>Created By</a>
+    </div>
+
     return(
-        <div className="relative h-20 w-screen bg-[#1c1c1c] flex justify-center items-center">
-            <p className="textShadow relative z-20 text-[#ffffff] font-bold text-3xl">State Fair<span className="font-light">dle</span></p>
-            <img src={headerBackground} className='max-w-xl border-b border-[#ffffff] w-screen mx-auto opacity-70 bottom-0 absolute left-0 right-0 z-10' />
+        <div className="relative h-20 w-screen bg-[#1c1c1c] flex items-center max-w-xl mx-auto">
+            <p className="textShadow relative z-20 text-[#ffffff] font-bold text-3xl pb-1 pl-4">State Fair<span className="font-light">dle</span></p>
+            {ShowAbout ? null : <div onClick={() => setShowAbout(true)} className='hover:cursor-pointer active:bg-opacity-90 absolute flex items-center justify-center text-3xl font-extrabold text-[#ffffff] bg-opacity-70 pb-1 right-4 shadow-lg h-12 w-12 rounded-full bg-[#404040] z-20'>?</div> }
+            <img src={headerBackground} className='shadow-lg w-screen mx-auto opacity-80 bottom-0 absolute left-0 right-0 z-10' />
+            {ShowAbout ? <About /> : null}
         </div>
     )
 }
