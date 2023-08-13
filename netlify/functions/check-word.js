39be93340c90ad95f1beda934fd410b79af80669
rossvasquez <ross.vasquez.17@gmail.com>
2023-08-13 00:18:42 -0500

    Updates

--- netlify/functions/check-word.js
+++ netlify/functions/check-word.js
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
         bool = false
     }
     
-    if (payload == "PETES") {
+    if (payload == "PETES" || payload == "YEOLD") {
         bool = true
     }
 
